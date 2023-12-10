7efc848756f8a9b6a5bdfd1111883af1856b8cb5
VVVladyslav <138609115+VVVladyslav@users.noreply.github.com>
2023-12-10 21:09:51 +0200

    Update Iterative.java

--- src/Iterative.java
+++ src/Iterative.java
@@ -1,20 +1,20 @@
 public class Iterative {
 
     public static int findNumber(int nNumberToSearch) {
-        int fibNumberMin2 = 0;
-        int fibNumberMin1 = 1;
-        int fibNumberToReturn = 0;
-
+        
         if (nNumberToSearch <= 1) {
             return nNumberToSearch;
         }
 
+        int fibNumberMin2 = 0;
+        int fibNumberMin1 = 1;
+        int fibNumberToReturn = 0;
+        
         for (int i = 2; i <= nNumberToSearch; i++) {
             fibNumberToReturn = fibNumberMin2 + fibNumberMin1;
             fibNumberMin2 = fibNumberMin1;
             fibNumberMin1 = fibNumberToReturn;
         }
-
         return fibNumberToReturn;
     }
 }
